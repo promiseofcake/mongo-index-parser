26dd9986868b2312ab49c9cb4370f35631a7adb2
Lucas Kacher <lucas@vsco.co>
2015-03-18 17:31:05 -0700

    Add file to assist in downloading the remote indices

---
+++ download_index.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Script used to SSH into a remote server with mongos access, dump the indexes in
+#JSON format, download locally over SCP for use with the ./indexer.js script
+#to output indexes in a format consumable for tracking changes to indexes in
+#GitHub
+
+echo 'Script used to dump remote Mongo indices, and download over SCP for local consumption'
+
+USER=$(whoami)   #User running command
+#REMOTE_HOST=''  #Remote host with mongos access to cluster of choice
+#MONGO_USER=''   #Mongo DB User
+#MONGO_PASS=''   #Mongo DB Password
+#MONGO_DB=''     #Mongo DB for which to get indexes
+
+set -exu
+
+# Ensure all variables are bound
+echo "$REMOTE_HOST $MONGO_USER $MONGO_PASS $MONGO_DB" > /dev/null 2>&1
+
+# Connect to remote host, export Index records and upload to S3
+ssh ${USER}@${REMOTE_HOST} -- "(mongoexport --authenticationDatabase admin --username ${MONGO_USER} \
+ --password ${MONGO_PASS} -d ${MONGO_DB} -c system.indexes --jsonArray -o ${MONGO_DB}.json)"
+
+# Download from Remote Server
+scp ${USER}@${REMOTE_HOST}:${MONGO_DB}.json .
+
+echo "${MONGO_DB} indexes downloaded successfully, please use ./indexer ${MONGO_DB}.json > ${MONGO_DB}.js"
+exit
